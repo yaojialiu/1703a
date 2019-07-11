bfb6041ca363b43ca07e61d430b47e468941dec8
yjl <1806816416@qq.com>
2019-07-11 08:18:22 +0800

    第一次

---
+++ src/main/java/com/yjl/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.yjl.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	@RequestMapping("list")
+	public String list() {
+		return "helloWord";
+	}
+}
